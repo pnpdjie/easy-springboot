7e7d758f4b1f09bedf405a8e06bfa1ca6377ab5f
Wang <542574439@qq.com>
2019-01-13 16:03:06 +0800

    hello springboot

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "hello";
+	}
+}
